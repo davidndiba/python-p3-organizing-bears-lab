d17b77e8fb6900ea710ceb3369a5043d62fe9994
David <david.ndiba@student.moringaschool.com>
2023-09-03 20:28:20 +0300

    bears_lab inser,select,create

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex) VALUES 
+("Mr.Chocolate","5","Female"),
+("Rowdy","12","Male"),
+("Tabitha","25","Female"),
+("Sergeant Brown","18","Male"),
+("Melissa","54","Female"),
+("Grinch","34","Male"),
+("Wendy","15","Female"),
+(Null,"56","Male")
+;
